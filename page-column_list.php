553da8753f9cd3510f8ba5f120b68ff7d67de1d6
mizow1 <skunk0915@gmail.com>
2025-05-08 18:55:50 +0900

    コラムリスト処理を共通化

--- page-column_list.php
+++ page-column_list.php
@@ -8,53 +8,8 @@ get_header(); ?>
 <div id="primary" class="site-content">
     <div id="content" role="main">
         <?php
-        // ページネーション対応
-        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-        // コラムカテゴリーの投稿を取得
-        $args = array(
-            'post_type'      => 'post',
-            'category_name'  => 'column',    // カテゴリーのスラッグ
-            'posts_per_page' => 10,
-            'paged'          => $paged,
-        );
-        $column_query = new WP_Query($args);
-
-        if ( $column_query->have_posts() ) : ?>
-
-            <ul class="column-list">
-            <?php while ( $column_query->have_posts() ) : $column_query->the_post(); ?>
-                <li class="column-item">
-                <a href="<?php the_permalink(); ?>">
-                    <?php if ( has_post_thumbnail() ) : ?>
-                        <a href="<?php the_permalink(); ?>" class="column-thumb">
-                            <?php the_post_thumbnail('thumbnail'); ?>
-                        </a>
-                    <?php endif; ?>
-                    <div class="column-entry">
-                        <div class="column-meta">
-                            <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date('Y.m.d'); ?></time>
-                        </div>
-                        <h2 class="column-title">
-                            <?php the_title(); ?>
-                        </h2>
-
-                    </div>
-                </li>
-                </a>
-            <?php endwhile; ?>
-            </ul>
-
-            <?php
-            // ページナビゲーション
-            the_posts_pagination( array(
-                'prev_text' => '« 前へ',
-                'next_text' => '次へ »',
-            ) );
-        else : ?>
-            <p>現在、コラムは投稿されていません。</p>
-        <?php endif;
-        wp_reset_postdata(); ?>
-
+        display_post_list(10, 'column', true);
+        ?>
     </div><!-- #content -->
 </div><!-- #primary -->
 
